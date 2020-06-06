1f7b7106fe8a49be881dd82eac14c48e4a167216
dariusdv <ddariusn@gmail.com>
2020-06-06 22:27:54 +0300

    Average Pair

--- index.js
+++ index.js
@@ -98,23 +98,50 @@
 //##################-> Are There Duplicates <-##################
 //##############################################################
 
-function areThereDuplicates(...args) {
-	args.sort((a, b) => a > b);
+// function areThereDuplicates(...args) {
+// 	args.sort((a, b) => a > b);
 
-	let start = 0;
-	let next = 1;
+// 	let start = 0;
+// 	let next = 1;
 
-	while (next < args.length) {
-		if (args[start] === args[next]) {
+// 	while (next < args.length) {
+// 		if (args[start] === args[next]) {
+// 			return true;
+// 		}
+// 		start++;
+// 		next++;
+// 	}
+
+// 	return false;
+// }
+
+// console.log(areThereDuplicates(1, 2, 3));
+// console.log(areThereDuplicates(1, 2, 2));
+// console.log(areThereDuplicates('a', 'a', 'c', 'a'));
+
+//######################################################
+//##################-> Average Pair <-##################
+//######################################################
+
+function averagePair(arr, avrg) {
+	if (arr.length === 0) return false;
+	let left = 0;
+	let right = arr.length - 1;
+
+	while (right > left) {
+		let average = (arr[right] + arr[left]) / 2;
+		if (average == avrg) {
 			return true;
+		} else if (average > avrg) {
+			right--;
+		} else {
+			left++;
 		}
-		start++;
-		next++;
 	}
-
 	return false;
 }
 
-console.log(areThereDuplicates(1, 2, 3));
-console.log(areThereDuplicates(1, 2, 2));
-console.log(areThereDuplicates('a', 'a', 'c', 'a'));
+console.log(averagePair([ 1, 2, 3 ], 2.5));
+console.log(averagePair([ 1, 3, 3, 5, 6, 7, 10, 12, 19 ], 8));
+console.log(averagePair([ -1, 0, 3, 4, 5, 6 ], 4.1));
+console.log(averagePair([], 4));
