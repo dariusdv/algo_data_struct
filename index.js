8967661b0ec341005665e7047c9e770eafe0cffa
dariusdv <ddariusn@gmail.com>
2020-06-06 23:19:26 +0300

    Is Subsequence & Max Sub-Array Sum & Min Sub-Array Length

--- index.js
+++ index.js
@@ -123,25 +123,110 @@
 //##################-> Average Pair <-##################
 //######################################################
 
-function averagePair(arr, avrg) {
-	if (arr.length === 0) return false;
-	let left = 0;
-	let right = arr.length - 1;
+// function averagePair(arr, avrg) {
+// 	if (arr.length === 0) return false;
+// 	let left = 0;
+// 	let right = arr.length - 1;
 
-	while (right > left) {
-		let average = (arr[right] + arr[left]) / 2;
-		if (average == avrg) {
-			return true;
-		} else if (average > avrg) {
-			right--;
+// 	while (right > left) {
+// 		let average = (arr[right] + arr[left]) / 2;
+// 		if (average == avrg) {
+// 			return true;
+// 		} else if (average > avrg) {
+// 			right--;
+// 		} else {
+// 			left++;
+// 		}
+// 	}
+// 	return false;
+// }
+
+// console.log(averagePair([ 1, 2, 3 ], 2.5));
+// console.log(averagePair([ 1, 3, 3, 5, 6, 7, 10, 12, 19 ], 8));
+// console.log(averagePair([ -1, 0, 3, 4, 5, 6 ], 4.1));
+// console.log(averagePair([], 4));
+
+//########################################################
+//##################-> Is Subsequence <-##################
+//########################################################
+
+function isSubsequence(firstStr, secondStr) {
+	let ptrFirst = 0;
+	let ptrSecond = 0;
+
+	while (ptrSecond < secondStr.length) {
+		if (firstStr[ptrFirst] === secondStr[ptrSecond]) {
+			ptrFirst++;
+			ptrSecond++;
 		} else {
-			left++;
+			ptrSecond++;
 		}
+		if (ptrFirst === firstStr.length) return true;
 	}
+
 	return false;
 }
 
-console.log(averagePair([ 1, 2, 3 ], 2.5));
-console.log(averagePair([ 1, 3, 3, 5, 6, 7, 10, 12, 19 ], 8));
-console.log(averagePair([ -1, 0, 3, 4, 5, 6 ], 4.1));
-console.log(averagePair([], 4));
+console.log(isSubsequence('hello', 'hello world'));
+console.log(isSubsequence('sing', 'sting'));
+console.log(isSubsequence('abc', 'abracadabra'));
+console.log(isSubsequence('abc', 'acb'));
+
+//##########################################################
+//##################-> Max Subarray Sum <-##################
+//##########################################################
+
+function maxSubarraySum(arr, num) {
+	if (arr.length < num) return null;
+
+	let total = 0;
+	for (let elem = 0; elem < num; elem++) {
+		total += arr[elem];
+	}
+	let currentTotal = total;
+	for (let elem = num; elem < arr.length; elem++) {
+		currentTotal += arr[elem] - arr[elem - num];
+		total = Math.max(total, currentTotal);
+	}
+	return total;
+}
+
+console.log(maxSubarraySum([ 100, 200, 300, 400 ], 2));
+console.log(maxSubarraySum([ 1, 4, 2, 10, 23, 3, 1, 0, 20 ], 4));
+console.log(maxSubarraySum([ -3, 4, 0, -2, 6, -1 ], 2));
+console.log(maxSubarraySum([ 3, -2, 7, -4, 1, -1, 4, -2, 1 ], 2));
+console.log(maxSubarraySum([ 2, 3 ], 3));
+
+//##########################################################
+//##################-> Min Subarray Sum <-##################
+//##########################################################
+
+function minSubArrayLen(arr, int) {
+	let sum = 0;
+	let start = 0;
+	let end = 0;
+	let minLen = Infinity;
+
+	while (start < arr.length) {
+		if (sum < int && end < arr.length) {
+			sum += arr[end];
+			end++;
+		} else if (sum >= int) {
+			minLen = Math.min(minLen, end - start);
+			sum -= arr[start];
+			start++;
+		} else {
+			break;
+		}
+	}
+
+	return minLen === Infinity ? 0 : minLen;
+}
+
+console.log(minSubArrayLen([ 2, 3, 1, 2, 4, 3 ], 7));
+console.log(minSubArrayLen([ 2, 1, 6, 5, 4 ], 9));
+console.log(minSubArrayLen([ 3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19 ], 52));
+console.log(minSubArrayLen([ 1, 4, 16, 22, 5, 7, 8, 9, 10 ], 39));
+console.log(minSubArrayLen([ 1, 4, 16, 22, 5, 7, 8, 9, 10 ], 55));
+console.log(minSubArrayLen([ 4, 3, 3, 8, 1, 2, 3 ], 11));
+console.log(minSubArrayLen([ 1, 4, 16, 22, 5, 7, 8, 9, 10 ], 95));
